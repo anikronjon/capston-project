247b925ea00367fc1f1df72ae320119a4eb38a79
anikronjon <anikronjon.swe@gmail.com>
2023-05-30 03:02:02 +0600

    update to bikash

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,14 +4,11 @@ from . import views
 app_name = 'mainapp'
 urlpatterns = [
     path('', views.home_view, name='home_page'),
-    path('about/', views.about_view, name='about_page'),
     path('service/', views.service_view, name='service_page'),
     path('project/', views.project_view, name='project_page'),
     path('contact/', views.contact_view, name='contact_page'),
     path('volunteer/', views.volunteer_view, name='volunteer'),
-    path('donate/', views.donate, name='paymentPage'),
-    path('donate/success/', views.donation_success, name='donation_success'),
-    path('donate/cancel/', views.donation_cancel, name='donation_cancel'),
-    path('checkout/', views.payment_checkout, name='payment'),
+    path('initiate-payment/', views.initiate_payment, name='initiate_payment'),
+    path('payment-status/', views.payment_status, name='payment_status'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,6 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-import stripe
-from .forms import DonationForm
-from .models import Donation
-
-stripe.api_key = settings.STRIPE_SECRET_KEY
+import requests
 
 
 def home_view(request):
@@ -15,10 +8,6 @@ def home_view(request):
         'user': request.user
     }
     return render(request, 'mainapp/index.html', {})
-
-
-def about_view(request):
-    return render(request, 'mainapp/about.html', {})
 
 
 def service_view(request):
@@ -37,88 +26,40 @@ def volunteer_view(request):
     return render(request, 'mainapp/volunteer.html', {})
 
 
-def donation_success(request):
-    return render(request, 'mainapp/success.html')
+def initiate_payment(request):
+    amount = 1000  # Example amount in BDT
 
+    # Construct the request payload
+    payload = {
+        'amount': amount,
+        # Add other required parameters
+    }
 
-def donation_cancel(request):
-    return render(request, 'mainapp/cancle.html')
+    # Send a request to the Bikash API for initiating the payment
+    response = requests.post(
+        'https://api.bikash.io/checkout/token/grant',  # Example endpoint URL
+        json=payload,
+        headers={
+            'MerchantId': 'YOUR_MERCHANT_ID',
+            'APIKey': 'YOUR_API_KEY',
+        }
+    )
 
-
-def donate(request):
-    if request.method == 'POST':
-        form = DonationForm(request.POST)
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            amount_in_cents = int(amount * 100)
-
-            session = stripe.checkout.Session.create(
-                payment_method_types=['card'],
-                line_items=[{
-                    'price_data': {
-                        'currency': 'usd',
-                        'product_data': {
-                            'name': 'Donation',
-                        },
-                        'unit_amount': amount_in_cents,
-                    },
-                    'quantity': 1,
-                }],
-                mode='payment',
-                success_url='http://127.0.0.1:8000/donate/success/',
-                cancel_url='http://127.0.0.1:8000/donate/cancel/',
-            )
-
-            return render(request, 'mainapp/donate_checkout.html', {'session_id': session.id})
-
+    # Process the response and extract the payment URL or other relevant data
+    if response.status_code == 200:
+        payment_url = response.json().get('payment_url')
+        # Redirect the user to the payment URL or render a template with the payment URL
+        return render(request, 'mainapp/donate.html', {'payment_url': payment_url})
     else:
-        form = DonationForm()
-
-    return render(request, 'mainapp/donate_form.html', {'form': form})
-
-
-# payment/views.py
+        # Handle the error case appropriately
+        return render(request, 'mainapp/error.html')
 
 
-def payment_checkout(request):
+def payment_status(request):
+    # Example implementation:
     if request.method == 'POST':
-        cart_items = request.POST.getlist('cart-item')
-        amount = 0
-        line_items = []
-
-        for item in cart_items:
-            # Calculate the total amount and construct the line items
-            # based on your application's logic
-            # Example: Get the product and price from the database
-            # and create the line item for Stripe
-
-            product_name = 'Product Name'  # Replace with actual product name
-            product_price = 1000  # Replace with actual product price in cents
-
-            amount += product_price
-
-            line_items.append({
-                'price_data': {
-                    'currency': 'usd',
-                    'unit_amount': product_price,
-                    'product_data': {
-                        'name': product_name,
-                    },
-                },
-                'quantity': 1,
-            })
-
-        try:
-            intent = stripe.PaymentIntent.create(
-                amount=amount,
-                currency='usd',
-                payment_method_types=['card'],
-                line_items=line_items,
-            )
-            return render(request, 'mainapp/donate_checkout.html', {'client_secret': intent.client_secret})
-
-        except stripe.error.StripeError as e:
-            # Handle Stripe errors
-            return render(request, 'mainapp/error.html', {'error': str(e)})
-
-    return render(request, 'mainapp/donate_checkout.html')
+        # Process the payment status notification
+        # Extract the relevant data from the request and update your models accordingly
+        return HttpResponse(status=200)
+    else:
+        return HttpResponseNotAllowed(['POST'])
